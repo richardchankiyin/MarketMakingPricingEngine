ff948438235ea21f7425b3257d4ba17f6029d60e
Richard Chan <richardchankiyin@gmail.com>
2026-04-27 17:25:54 +0100

    minor update on PricingEngine and its test case

--- pricingcomponent/src/main/java/com/richard/marketmakingpricing/PricingEngine.java
+++ pricingcomponent/src/main/java/com/richard/marketmakingpricing/PricingEngine.java
@@ -1,94 +1,75 @@
 package com.richard.marketmakingpricing;
 
-import java.util.concurrent.atomic.AtomicReference;
-
-public class PricingEngine implements MarketUpdateListener {
-
+public class PricingEngine implements MarketUpdateListener, SignalListener {
     private final SignalEmitter signalEmitter;
-    private final LocalBookView localBook = new LocalBookView();
-    private final AtomicReference<Quote> currentActiveQuote = new AtomicReference<>();
+    private final PriceAggregator aggregator;
     
-    private final Quote[] quotePool;
-    private int poolIndex = 0;
-    private static final int POOL_SIZE = 1024;
+    private final double tickSize;
+    private final double minProfitMargin;
+    private final double maxSignalSkew;
 
-    private static final double DEFAULT_SPREAD = 0.02;
-    private static final int MY_QUOTE_SIZE = 1000;
-    private static final double TICK_SIZE = 0.001;
+    private volatile double currentSignal = 0.0;
+    private volatile double bestBid = 0.0;
+    private volatile double bestAsk = 0.0;
 
-    public PricingEngine(PriceAggregator aggregator, SignalEmitter signalEmitter) {
+    private double myBid;
+    private double myAsk;
+
+    public PricingEngine(PriceAggregator aggregator, SignalEmitter signalEmitter, 
+                         double tickSize, double minProfitMargin, double maxSignalSkew) {
+        this.aggregator = aggregator;
         this.signalEmitter = signalEmitter;
-        this.quotePool = new Quote[POOL_SIZE];
-        for (int i = 0; i < POOL_SIZE; i++) {
-            quotePool[i] = new Quote();
-        }
+        this.tickSize = tickSize;
+        this.minProfitMargin = minProfitMargin;
+        this.maxSignalSkew = maxSignalSkew;
 
-        aggregator.setListener(this);
-        signalEmitter.setListener(this::onSignalUpdate);
+        this.aggregator.setListener(this);
+        this.signalEmitter.setListener(this);
     }
 
     @Override
-    public void onBookUpdate(double bid, int bSize, double ask, int aSize,  double vwapBid, double vwapAsk) {
-        localBook.update(bid, bSize, ask, aSize);
+    public void onSignalChange(double newSignal) {
+        this.currentSignal = newSignal;
         refreshQuote();
     }
 
-    public void onSignalUpdate(double newSignal) {
+    @Override
+    public void onBookUpdate(double bid, int bSize, double ask, int aSize, double vwapBid, double vwapAsk) {
+        this.bestBid = bid;
+        this.bestAsk = ask;
         refreshQuote();
     }
 
     private void refreshQuote() {
-        if (localBook.bestBid <= 0 || localBook.bestAsk >= Double.MAX_VALUE) return;
+        if (bestBid <= 0 || bestAsk <= 0 || bestBid >= bestAsk) return;
 
-        // 1. Calculate Weighted Mid (Micro-Price)
-        // This uses the bid/ask sizes to see which way the market is leaning.
-        double totalVolume = (double) localBook.bidSize + localBook.askSize;
-        double imbalanceWeight = localBook.bidSize / totalVolume;
-        // If bidSize is huge, microMid will be closer to the Ask (Bullish)
-        double microMid = (localBook.bestBid * (1 - imbalanceWeight)) + (localBook.bestAsk * imbalanceWeight);
+        // 1. Anchor to the Current Market Mid
+        double mid = (bestBid + bestAsk) / 2.0;
 
-        // 2. Incorporate Signal Skew
-        double signal = signalEmitter.getCurrentSignal();
-        double fairValue = microMid + (signal * 0.05);
+        // 2. Skew Fair Value based on Alpha (Signal)
+        double fairValue = mid + (currentSignal * maxSignalSkew);
 
-        // 3. Dynamic Spread adjustment based on Liquidity
-        // If liquidity is low (volatile), we widen the spread to protect ourselves.
-        double spreadBuffer = (totalVolume < 500) ? DEFAULT_SPREAD * 1.5 : DEFAULT_SPREAD;
+        // 3. Determine IDEAL Quote (Spread around Skewed Fair Value)
+        double idealBid = fairValue - (minProfitMargin / 2.0);
+        double idealAsk = fairValue + (minProfitMargin / 2.0);
 
-        double myBid = roundToTick(fairValue - (spreadBuffer / 2.0));
-        double myAsk = roundToTick(fairValue + (spreadBuffer / 2.0));
+        // 4. Apply PROFITABILITY CLAMP (The Reality Check)
+        // We MUST buy for less than we sell to LP (bestBid)
+        // We MUST sell for more than we buy from LP (bestAsk)
+        double clampedBid = Math.min(idealBid, bestBid - tickSize);
+        double clampedAsk = Math.max(idealAsk, bestAsk + tickSize);
 
-        // 4. Competitive boundaries (Price Improvement)
-        // We try to "join" the best bid/ask or beat it by one tick if we want to be aggressive
-        if (myBid >= localBook.bestAsk) myBid = localBook.bestAsk - TICK_SIZE;
-        if (myAsk <= localBook.bestBid) myAsk = localBook.bestBid + TICK_SIZE;
+        // 5. High-Precision Tick Rounding
+        double scale = 1.0 / tickSize;
+        this.myBid = Math.round(clampedBid * scale) / scale;
+        this.myAsk = Math.round(clampedAsk * scale) / scale;
 
-        // 5. Publish
-        Quote nextQuote = quotePool[poolIndex++ & (POOL_SIZE - 1)];
-        nextQuote.update(myBid, MY_QUOTE_SIZE, myAsk, MY_QUOTE_SIZE, System.currentTimeMillis());
-
-        currentActiveQuote.set(nextQuote);
-    }
-
-    private double roundToTick(double value) {
-        return Math.round(value / TICK_SIZE) * TICK_SIZE;
-    }
-
-    public Quote getCurrentQuote() {
-        return currentActiveQuote.get();
-    }
-
-    private static class LocalBookView {
-        double bestBid;
-        int bidSize;
-        double bestAsk;
-        int askSize;
-
-        void update(double b, int bs, double a, int as) {
-            this.bestBid = b;
-            this.bidSize = bs;
-            this.bestAsk = a;
-            this.askSize = as;
+        // 6. Safety Guard (Never cross own spread)
+        if (this.myBid >= this.myAsk) {
+            this.myBid = this.myAsk - tickSize;
         }
     }
+
+    public double getMyBid() { return myBid; }
+    public double getMyAsk() { return myAsk; }
 }
